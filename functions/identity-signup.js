dd8f5bb3d21a0dfb47e10da21f60d3bc5ed70cd8
caoxuanphong <caoxuanphong.khtn@gmail.com>
2023-09-26 16:45:40 +0700

    Update identity-signup.js

--- functions/identity-signup.js
+++ functions/identity-signup.js
@@ -9,10 +9,10 @@ exports.handler = async (event) => {
   const customer = await stripe.customers.create({ email: user.email });
 
   // subscribe the new customer to the free plan
-  await stripe.subscriptions.create({
-    customer: customer.id,
-    items: [{ price: process.env.STRIPE_DEFAULT_PRICE_PLAN }],
-  });
+  // await stripe.subscriptions.create({
+  //   customer: customer.id,
+  //   items: [{ price: process.env.STRIPE_DEFAULT_PRICE_PLAN }],
+  // });
 
   // store the Netlify and Stripe IDs in Fauna
   await faunaFetch({
