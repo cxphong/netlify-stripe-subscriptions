69083836d7ad7afc3e227b02867340d8a4820c3e
Phong Cao <phongcao@degreeperfect.com>
2023-09-28 15:34:09 +0700

    UPdate

--- functions/create-manage-link.js
+++ functions/create-manage-link.js
@@ -4,23 +4,6 @@ const { faunaFetch } = require('./utils/fauna');
 exports.handler = async (_event, context) => {
   const { user } = context.clientContext;
 
-  // const result = await faunaFetch({
-  //   query: `
-  //     query ($netlifyID: ID!) {
-  //       getUserByNetlifyID(netlifyID: $netlifyID) {
-  //         stripeID
-  //       }
-  //     }
-  //   `,
-  //   variables: {
-  //     netlifyID: user.sub,
-  //   },
-  // });
-
-  //console.log(result);
-
-  // const { stripeID } = result.data.getUserByNetlifyID;
-
   const customer = await stripe.customers.create({ email: "caoxuanphong.khtn@gmail.com" });
   const link = await stripe.billingPortal.sessions.create({
     customer: customer.id,
